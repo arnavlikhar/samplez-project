facae6402f06df2ebfdd263ded33b82d6247c016
arnavlikhar <74099853+arnavlikhar@users.noreply.github.com>
2021-06-09 22:02:41 +0530

    Add files via upload

---
+++ samplez project.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import csv
+import random
+import plotly.figure_factory as ff
+import statistics
+import plotly.graph_objects as go
+df=pd.read_csv("medium_data.csv")
+data=df["reading_time"].tolist()
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index= random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+mean_list=[]
+for i in range(0,1000):
+    set_of_means=random_set_of_mean(100)
+    mean_list.append(set_of_means)
+mean=statistics.mean(data)
+std_deviation=statistics.stdev(data)
+fig = ff.create_distplot([mean_list], ["responses"], show_hist=False)
+print (mean)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN"))
+first_std_deviation_start,first_std_deviation_end=mean-std_deviation,mean+std_deviation
+second_std_deviation_start,second_std_deviation_end=mean-(2*std_deviation),mean+(2*std_deviation)
+third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation), mean+(3*std_deviation)
+fig.add_trace(go.Scatter(x=[first_std_deviation_start,first_std_deviation_start],y=[0,0.17],mode="lines",name="1hp"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_start,second_std_deviation_start],y=[0,0.17],mode="lines",name="2hp"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_start,third_std_deviation_start],y=[0,0.17],mode="lines",name="3hp"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_end,first_std_deviation_end],y=[0,0.17],mode="lines",name="1hp"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_end,second_std_deviation_end],y=[0,0.17],mode="lines",name="2hp"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_end,third_std_deviation_end],y=[0,0.17],mode="lines",name="3hp"))
+fig.show()
